e4a2f4f28d5c31b78fddfbcd6778d2ae9609067e
RitaRublevskaya06 <144614540+RitaRublevskaya06@users.noreply.github.com>
2024-08-20 18:57:15 +0300

    Add files via upload

---
+++ OAIP/2sem/OAIP_Lab_09/Dop1/main.cpp
@@ -0,0 +1,137 @@
+#include "List.h"
+#include <iostream>
+using namespace std;
+
+
+bool Object::Insert(void* data)           // Вставка в начало
+{
+	bool rc = 0;
+	if (Head == NULL)
+	{
+		Head = new Element(NULL, data, Head);
+		rc = true;
+	}
+	else
+	{
+		Head = (Head->Prev = new Element(NULL, data, Head));
+		rc = true;
+	}
+	return rc;
+}
+//-----------------------------------------------------------
+Element* Object::Search(void* data)    // Найти заданный элемент
+{
+	Element* rc = Head;
+	while ((rc != NULL) && (rc->Data != data))
+		rc = rc->Next;
+	return rc;
+}
+//-----------------------------------------------------------
+void Object::PrintList(void(*f)(void*))     // Вывод
+{
+	Element* e = Head;
+	while (e != NULL)
+	{
+		f(e->Data);
+		e = e->GetNext();
+	};
+}
+//-----------------------------------------------------------
+void Object::PrintList(void(*f)(void*), Element* e)
+{
+	f(e->Data);
+}
+//-----------------------------------------------------------
+bool  Object::Delete(Element* e)    // Удалить по ссылке
+{
+	bool rc = 0;
+	if (rc = (e != NULL))
+	{
+		if (e->Next != NULL)
+			e->Next->Prev = e->Prev;
+		if (e->Prev != NULL)
+			e->Prev->Next = e->Next;
+		else
+			Head = e->Next;
+		delete e;
+	}
+	return rc;
+}
+//-----------------------------------------------------------
+bool Object::Delete(void* data)      // Удалить по значению
+{
+	return Delete(Search(data));
+};
+//-----------------------------------------------------------
+Element* Object::GetLast()
+{
+	Element* e = Head, * rc = e;
+	while (e != NULL)
+	{
+		rc = e;
+		e = e->GetNext();
+	}
+	return rc;
+}
+//-----------------------------------------------------------
+Object Create()
+{
+	return *(new Object());
+}
+
+struct Person
+{
+	char name[20];
+	int age;
+	Person* next;
+};
+
+void print(void* b)       //Функция используется при выводе 
+{
+	Person* a = (Person*)b;
+	cout << a->name << "  " << a->age << endl;
+}
+void Object::deleteList()
+{
+	while (Head != nullptr)
+	{
+		Element* temp = Head;
+		Head = Head->Next;
+		delete temp;
+	}
+}
+
+int Object::countList()
+{
+	int count = 0;
+	Element* current = Head;
+	while (current != nullptr)
+	{
+		count++;
+		current = current->Next;
+	}
+	return count;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	Person a1 = { "Петров", 20 };
+	Person a2 = { "Сидоров", 25 };
+	Person a3 = { "Гончаров", 47 };
+	bool rc;
+	Object L1 = Create();   // создание списка с именем L1
+	L1.Insert(&a1);
+	L1.Insert(&a2);
+	L1.Insert(&a3);
+	L1.PrintList(print);
+	Element* e = L1.Search(&a3);
+	Person* aa = (Person*)e->Data;
+	cout << "Найден элемент= " << aa->name << endl;
+	rc = L1.Delete(&a2);
+	if (rc == true) cout << "Удален элемент" << endl;
+	cout << "Список:" << endl;
+	L1.PrintList(print);
+	return 0;
+}
+
